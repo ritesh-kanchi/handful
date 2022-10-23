f79de8ec9e58f7246a3c5e905f4b351e5b9f5874
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2022-10-22 20:44:28 -0700

    Initial Commit

---
+++ handful/handfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  handfulApp.swift
+//  handful
+//
+//  Created by Ritesh Kanchi on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct handfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
